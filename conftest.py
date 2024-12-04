398a118bc5635e1253c09283db887802dea5c48d
Elena Filatova <elenka_nikky@mail.ru>
2024-12-04 15:38:44 +0300

    add logging

--- conftest.py
+++ conftest.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
 import pytest
+import logging
+from datetime import datetime
+import os
+
+
+# Configure logging
+logging.basicConfig(filename=os.path.join("./logs", 'test_log.log'),
+                    level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 
 def pytest_addoption(parser):
     parser.addoption("--browser", "-B", action="store", default="chrome", help="choose your browser")
+    parser.addoption("--log_level", action="store", default="INFO")
     
 # задаем браузер через cli
 @pytest.fixture(autouse=True)
@@ -21,3 +32,19 @@ def driver(request):
 
     request.cls.driver = driver
     request.addfinalizer(driver.close)
+
+    log_level = request.config.getoption("--log_level")
+    logger = logging.getLogger('driver')
+    test_name = request.node.name
+    if "\\" in test_name:
+        test_name = test_name.split("\\")[0]
+    log_path = f"logs/{test_name}_{datetime.now().strftime('%d-%m-%Y_%H.%M.%S')}.log"
+
+    logger.addHandler(logging.FileHandler(log_path))
+    logger.setLevel(level=log_level)
+    logger.info(f"{logger.name} ===> Test {test_name} started at {datetime.now()}")
+
+    driver.test_name = test_name
+    driver.log_level = log_level
+    driver.log_path = log_path
+    logger.info(f"Browser: {request.config.getoption('--browser')} ")
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 from pages.login_page import LoginPage
+import logging
 
 class TestLoginPage:
 
@@ -9,9 +10,12 @@ class TestLoginPage:
 
     @pytest.mark.parametrize('login, password', [('test','test'), ('test1','test1'), ('test2','tes2t')])
     def test_login_in_account(self, login, password):
+        logging.info("test_login_in_account has started")
         self.login_page.open()
         self.login_page.enter_login(login)
         self.login_page.enter_password(password)
         self.login_page.click_submit_button()
         time.sleep(1)
         assert self.login_page.error_validation, "Нет ошибки валидации поля email"
+        logging.info("Login successful")
+        
